3c438b15c1a4b6d270a158d9bd1f29f26df9915e
kyle_N <kylecnovak@gmail.com>
2018-05-30 18:30:02 -0700

    Revert "5/30 Commit for Dead Week ERD lessons, some jQuery assignments, and SQL work in general which we never got to during the WebDev weeks."
    This reverts commit 49906f830737070ff509d7795a679aa8a02c9d89.

--- MySQL_Queries/Countries_SQL.sql
+++
@@ -1,6 +0,0 @@
-SELECT countries.name, languages.language, languages.percentage
-FROM languages
-JOIN countries ON languages.country_id = countries.id
-WHERE languages.language = 'Slovene'
-ORDER BY languages.percentage DESC;
-
